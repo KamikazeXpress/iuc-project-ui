159e83ce697ad1db29bc3f61100a395e4f1d3e05
kdhanyan <kuhan.dhanyan@intel.com>
2021-10-28 16:11:55 +0800

    addin in firebasex push notifacation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FirebaseX } from '@ionic-native/firebase-x/ngx';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor(private firebaseX: FirebaseX) { }
+  ngOnInit(): void {
+    this.firebaseX.getToken()
+      .then(token => console.log(`The token is ${token}`)) // save the token server-side and use it to push notifications to this device
+      .catch(error => console.error('Error getting token', error));
+
+    this.firebaseX.onMessageReceived()
+      .subscribe(data => console.log(`User opened a notification ${data}`));
+
+    this.firebaseX.onTokenRefresh()
+      .subscribe((token: string) => console.log(`Got a new token ${token}`));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-
+import { FirebaseX } from '@ionic-native/firebase-x/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -21,7 +21,7 @@ import { environment } from '../environments/environment';
   // or after 30 seconds (whichever comes first).
   registrationStrategy: 'registerWhenStable:30000'
 })],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy, }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },FirebaseX],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
