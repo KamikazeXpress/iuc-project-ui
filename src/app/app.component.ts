8d9ada50015f577b729d99b83acd02f65a9743f3
kdhanyan <kuhan.dhanyan@intel.com>
2021-11-02 15:38:11 +0000

    added push notification support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, } from '@angular/core';
+import { FirebaseX } from "@ionic-native/firebase-x/ngx";
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private platform: Platform, private firebase: FirebaseX) {
+
+    this.initializeApp();
+
+  }
+  initializeApp() {
+    this.platform.ready().then(() => {
+
+      this.firebase.getToken()
+        .then(token => console.log(`The token is ${token}`)) // save the token server-side and use it to push notifications to this device
+        .catch(error => console.error('Error getting token', error));
+
+      this.firebase.onMessageReceived()
+        .subscribe(data => console.log(`User opened a notification ${data}`));
+
+      this.firebase.onTokenRefresh()
+        .subscribe((token: string) => console.log(`Got a new token ${token}`));
+    });
+  }
+
+
+
 }
