abe1f6c7e2ce5be32af94a8e545e8010a0f8597b
PUTHENESWARAN SELVAKUMARAN <putheneswaran.selvakumaran@intel.com>
2021-11-02 12:57:25 +0800

    added dashboard page

---
+++ src/app/tab4/tab4.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
+import { User } from '../User';
+
+@Component({
+  selector: 'app-tab4',
+  templateUrl: './tab4.page.html',
+  styleUrls: ['./tab4.page.scss'],
+})
+export class Tab4Page implements OnInit {
+
+  usersinfo: User;
+
+  constructor(private dataService: DataService) { 
+    this.usersinfo = new User();
+  }
+
+  ngOnInit() {
+    this.dataService.getLocalDataUsers('user1').subscribe((data: User) => {
+    this.usersinfo = data;
+    })
+  }
+
+}
